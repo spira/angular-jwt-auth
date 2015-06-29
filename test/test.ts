7ee37dead64f384b651b324bd6b3c6ca5a59b068
Zak Henry <zak.henry@gmail.com>
2015-06-29 22:40:18 +1000

    Fixed .editorconfig indent definition and the test.ts indentation

--- test/test.ts
+++ test/test.ts
@@ -5,38 +5,38 @@ var expect = chai.expect;
 
 describe('Service Provider Tests', () => {
 
-  var ngJwtAuthServiceProviderObj;
+    var ngJwtAuthServiceProviderObj;
 
-  describe('Configuration', () => {
+    describe('Configuration', () => {
 
 
-    beforeEach(function () {
+        beforeEach(function () {
 
-      angular.module("providers", ['ngJwtAuth']); //require the module as dependency
-      module("providers"); //mock the depending module
+            angular.module("providers", ['ngJwtAuth']); //require the module as dependency
+            module("providers"); //mock the depending module
 
-      module((ngJwtAuthServiceProvider) => {
-        ngJwtAuthServiceProviderObj = ngJwtAuthServiceProvider; //register injection of service provider
-      });
+            module((ngJwtAuthServiceProvider) => {
+                ngJwtAuthServiceProviderObj = ngJwtAuthServiceProvider; //register injection of service provider
+            });
 
-      inject(); //complete injection
+            inject(); //complete injection
+        });
+
+
+        it('should configure api endpoints', function () {
+
+            ngJwtAuthServiceProviderObj.setApiEndpoints({
+                base: 'mock/base/path/',
+                login: 'mock/login',
+                refresh: 'mock/refresh'
+            });
+
+            expect(ngJwtAuthServiceProviderObj.apiEndpoints.base).to.equal('mock/base/path/');
+            expect(ngJwtAuthServiceProviderObj.apiEndpoints.login).to.equal('mock/login');
+            expect(ngJwtAuthServiceProviderObj.apiEndpoints.refresh).to.equal('mock/refresh');
+        });
+
     });
-
-
-    it('should configure api endpoints', function () {
-
-      ngJwtAuthServiceProviderObj.setApiEndpoints({
-        base: 'mock/base/path/',
-        login: 'mock/login',
-        refresh: 'mock/refresh'
-      });
-
-      expect(ngJwtAuthServiceProviderObj.apiEndpoints.base).to.equal('mock/base/path/');
-      expect(ngJwtAuthServiceProviderObj.apiEndpoints.login).to.equal('mock/login');
-      expect(ngJwtAuthServiceProviderObj.apiEndpoints.refresh).to.equal('mock/refresh');
-    });
-
-  });
 
 
 });
